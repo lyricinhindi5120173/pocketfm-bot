e286fe337f140a799d6c921e26ca9d34b469381e
lyricinhindi5120173 <lyricinhindi5120173@gmail.com>
2026-06-05 08:12:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 # SYSTEM CREDENTIALS & SECURITY ACCESS CONTROL LIST
 # =====================================================================
 
+TELEGRAM_BOT_TOKEN = "8663514965:AAGTjZJ4OYohgUhTyk7_75TabHU3H2Hpzek"
 
 # 🔐 Add your numeric Telegram User ID here. You can add trusted friends too.
 # Example: APPROVED_USERS = [123456789, 987654321]
